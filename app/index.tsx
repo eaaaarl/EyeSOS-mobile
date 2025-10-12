647843d2a7c985bffbc1730ed3bc5f3b26e363e3
eaaaarl <dev.earl00@gmail.com>
2025-10-12 17:43:59 +0800

    Design profile screen

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 export default function index() {
   return (
     <SafeAreaView className='flex-1 justify-center items-center'>
-      <Link className='text-center text-black' href={'/(auth)/welcome'}>GO TO WELCOME</Link>
+      <Link className='text-center font-semibold' href={'/(auth)/welcome'}>GO TO WELCOME</Link>
     </SafeAreaView>
   )
 }
